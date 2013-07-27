4578a6fc425e7081a63391ba357a55bf1c65c6ca
Bart Riemens <briemens@crafity.com>
2013-07-27 09:40:24 +0200

    Updated Package unit tests

--- test/package.test.js
+++ test/package.test.js
@@ -1,71 +1,66 @@
-/*jslint node:true, white:true, stupid: true */
+/*jslint node: true, bitwise: true, unparam: true, maxerr: 50, white: true, stupid: true */
+"use strict";
+
 /*!
- * package.test - package.json tests
- * Copyright(c) 2011 Crafity
- * Copyright(c) 2012 Galina Slavova
- * Copyright(c) 2012 Bart Riemens
+ * crafity-nodemanager - Node Module Package Tests
+ * Copyright(c) 2013 Crafity
+ * Copyright(c) 2013 Bart Riemens
+ * Copyright(c) 2013 Galina Slavova
  * MIT Licensed
  */
 
 /**
  * Test dependencies.
  */
-var jstest = require('crafity-jstest')
-	, assert = jstest.assert
-	, context = jstest.createContext("Module Package tests")
-	, fs = require('fs')
-	, main
-	;
 
-(function packageTests() {
-	"use strict";
+var jstest = require('crafity-jstest').createContext("Module Package Tests")
+  , assert = jstest.assert
+  , fs = require('fs')
+  ;
 
-	/**
-	 * The tests
-	 */
-	var tests = {
+/**
+ * Run the tests
+ */
+jstest.run({
 
-		'The module must have main.js file': function () {
+  'The module must have main file': function () {
 
-			main = require('../main');
-			assert.isDefined(main, "Expected main to be defined");
-		},
+    var main = require('../main');
+    assert.isDefined(main, "Expected main to be defined");
+  },
 
-		'The module must have a fullname': function () {
-			assert.isDefined(main.fullname, "Expected fullname to be defined");
-		},
+  'The module must have a fullname': function () {
+    var main = require('../main');
+    assert.isDefined(main.fullname, "Expected fullname to be defined");
+  },
 
-		'The module must have a version number': function () {
-			assert.isDefined(main.version, "Expected version number to be defined");
-		},
+  'The module must have a version number': function () {
+    var main = require('../main');
+    assert.isDefined(main.version, "Expected version number to be defined");
+  },
 
-		'The module must have package.json file': function () {
-			fs.readFileSync("./package.json");
-		},
+  'The module must have package.json file': function (context) {
+    fs.readFileSync("./package.json");
+  },
 
-		'The module must have the same name as quoted in package.json': function () {
+  'The module must have the same name as quoted in package.json': function () {
 
-			var data = fs.readFileSync("./package.json")
-				, json = JSON.parse(data.toString());
+    var main = require('../main')
+      , data = fs.readFileSync("./package.json")
+      , json = JSON.parse(data.toString());
 
-			assert.areEqual(json.name, main.fullname, "Expected module name to be the same in both places.");
+    assert.areEqual(json.name, main.fullname, "Expected module name to be the same in both places.");
 
-		},
+  },
 
-		'The module must have the same version as quoted in package.json': function () {
+  'The module must have the same version as quoted in package.json': function () {
 
-			var data = fs.readFileSync("./package.json")
-				, json = JSON.parse(data.toString());
+    var main = require('../main')
+      , data = fs.readFileSync("./package.json")
+      , json = JSON.parse(data.toString());
 
-			assert.isDefined(json.version, "Expected fs to be defined");
-			assert.areEqual(main.version, json.version, "Expected the same module version!");
-		}
+    assert.isDefined(json.version, "Expected fs to be defined");
+    assert.areEqual(main.version, json.version, "Expected the same module version!");
+  }
 
-	};
-
-	/**
-	 * Run the tests
-	 */
-	context.run(tests);
-
-}());
+});
